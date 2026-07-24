17e78ead5526342d92f4bf2652de8418f27cd7d3
SharjilSharma <sharjillegit@gmail.com>
2026-07-24 22:56:11 +0530

    Fix Gradio API parameters and add HF Token authentication

--- src/app/(protected)/try-on/page.tsx
+++ src/app/(protected)/try-on/page.tsx
@@ -175,8 +175,10 @@ export default function TryOnPage() {
     })
 
     try {
-      const { Client, handle_file } = await import('@gradio/client')
-      const client = await Client.connect(process.env.NEXT_PUBLIC_HF_SPACE_ID!)
+      const { Client } = await import('@gradio/client')
+      const client = await Client.connect(process.env.NEXT_PUBLIC_HF_SPACE_ID!, {
+        hf_token: process.env.NEXT_PUBLIC_HF_TOKEN as any
+      })
 
       // Fetch front garment blob
       const frontGarmentBlob = await getBlobFromSource(garmentFrontFile, selectedGarmentFrontUrl)
